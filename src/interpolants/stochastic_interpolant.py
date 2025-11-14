52249f09777b3855a0ab81e9dbb30aae574a05d3
Aaron Dinesh <aarondinesh2002@gmail.com>
2025-11-14 21:17:15 +0100

    Added the start of an SDE Integrator

--- src/interpolants/stochastic_interpolant.py
+++ src/interpolants/stochastic_interpolant.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from typing import Callable, Literal
 
 import jax
@@ -9,6 +10,8 @@ from .utils import make_gamma
 InterpolantCoefficient = Callable[[float], Array]
 TimeSchedule = Literal["linear", "cosine", "power"]
 GammaType = Literal["brownian", "a-brownian", "zero", "bsquared", "sinesquared", "sigmoid"]
+Velocity = jax.nnx.Module
+Score = jax.nnx.Module
 
 
 class LinearInterpolant:
@@ -38,3 +41,46 @@ class LinearInterpolant:
         z = jax.random.normal(key, x0.shape[0])
         t = jnp.broadcast_to(t, (x0.shape[0], 1, 1, 1))
         return self.interpolant(x0, x1, t) + self.gamma(t) * z, z
+
+
+@dataclass
+class SDEIntegrator:
+    b: Velocity
+    s: Score
+    eps: Array
+    interpolant: LinearInterpolant
+    n_save: int = 4
+    start_end: tuple[int, int] = (0, 1)  # pyright: ignore[reportIndexIssue]
+    n_step: int = 3000
+    n_likelihood: int = 1
+
+    def __post_init__(self) -> None:
+        """Initialize forward dynamics, reverse dynamics, and likelihood."""
+
+        def bf(x: Array, t: Array):
+            """Forward drift. Assume x is batched but t is not."""
+            self.b.to(x.device)
+            self.s.to(x.device)
+            return self.b(x, t) + self.eps * self.s(x, t)
+
+        def br(x: Array, t: Array):
+            """Backwards drift. Assume x is batched but t is not."""
+            self.b.to(x.device)
+            self.s.to(x.device)
+
+            return self.b(x, t) - self.eps * self.s(x, t)
+
+        def dt_logp(x: torch.tensor, t: torch.tensor):
+            """Time derivative of the log-likelihood, assumed integrating from 1 to 0.
+            Assume x is batched but t is not.
+            """
+            score = self.s(x, t)
+            s_norm = jnp.linalg.norm(score, axis=-1) ** 2
+            return -(compute_div(self.bf, x, t) + self.eps * s_norm)
+
+        self.bf = bf
+        self.br = br
+        self.dt_logp = dt_logp
+        self.start, self.end = self.start_end[0], self.start_end[1]
+        self.ts = torch.linspace(self.start, self.end, self.n_step)
+        self.dt = self.ts[1] - self.ts[0]
