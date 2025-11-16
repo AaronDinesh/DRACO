ba6e9cbddc3820ccb723459622c4ad8cc34eb06f
Aaron Dinesh <aarondinesh2002@gmail.com>
2025-11-16 17:47:47 +0000

    Fixed import issue in __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,5 @@
 from .gan.discriminator import Discriminator
 from .gan.generator import Generator
-from .interpolants import StochasticInterpolantModel
 from .typing import Batch, Loader
 from .utils import batch_metrics, make_transform, restore_checkpoint, save_checkpoint
 
