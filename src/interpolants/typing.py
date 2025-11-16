b3ec9f305c62581456c286335d6eb664be3e965a
Aaron Dinesh <aarondinesh2002@gmail.com>
2025-11-16 19:08:37 +0100

    Fixed bug in type hinting

--- src/interpolants/typing.py
+++ src/interpolants/typing.py
@@ -1,11 +1,12 @@
 from typing import Callable, Literal
 
 import jax
+from flax import nnx
 from jax import Array
 
 InterpolantCoefficient = Callable[[float], Array]
 TimeSchedule = Literal["linear", "cosine", "power"]
 GammaType = Literal["brownian", "a-brownian", "zero", "bsquared", "sinesquared", "sigmoid"]
-Velocity = jax.nnx.Module
-Score = jax.nnx.Module
+Velocity = nnx.Module
+Score = nnx.Module
 Array = jax.Array
